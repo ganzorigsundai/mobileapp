af69d717fa8a1b48820d38f4a78b0d8bbd6b3a10
ganzorigsundai <s247064@gs.sundaidenshi.ac.jp>
2025-10-17 10:33:32 +0900

    Delete top-page sample directory

--- top-page sample/js/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const items = document.querySelectorAll('.item01, .item02, .item03, .item04');
-    items.forEach((item, index) => {
-      setTimeout(() => {
-        item.classList.add('fadedown');
-      }, 200 * index);
-    });
-  });
